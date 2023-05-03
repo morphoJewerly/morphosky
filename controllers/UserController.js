395711ae12f77f643b2951f1ac543363a45798ca
Greg Ivanchuk <gregiv99gmail.com>
2023-05-03 18:18:01 +0300

    update bcryptjs

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,11 +1,11 @@
 import jwt from "jsonwebtoken";
-import bcrypt from "bcrypt";
+import bcryptjs from "bcryptjs";
 import UserModel from "../models/User.js";
 export const register = async (req,res)=> {
     try {
     const password = req.body.password;
-    const salt = await bcrypt.genSalt(10);
-    const hash = await bcrypt.hash(password,salt);
+    const salt = await bcryptjs.genSalt(10);
+    const hash = await bcryptjs.hash(password,salt);
     const doc = new UserModel ({
     email : req.body.email,
     fullName : req.body.fullName,
@@ -35,7 +35,7 @@ export const login = async (req,res)=> {
    const user = await UserModel.findOne({email : req.body.email})
    if (!user)  return req.status(404).json({message:"Користувача не знайдено!"})
    
-   const isValidPass = await bcrypt.compare(req.body.password , user._doc.passwordHash)
+   const isValidPass = await bcryptjs.compare(req.body.password , user._doc.passwordHash)
    if (!isValidPass)  return res.status(400).json({ message:"Користувача не знайдено!" })
 
    const token = jwt.sign({
