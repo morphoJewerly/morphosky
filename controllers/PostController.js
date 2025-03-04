9c6caf762a82b93c0308a1a766f37afaf3568980
arturysh <artur2002236@gmail.com>
2025-03-04 19:05:06 +0200

    add logic for variants goods

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -1,99 +1,131 @@
-import PostModel from "../models/Post.js"
+import PostModel from "../models/Post.js";
+
 export const getAll = async (req, res) => {
-  try {
-    const posts = await PostModel.find();
-    res.json(posts);
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({
-      message: "Невдалося знайти товари",
-    });
-  }
-}
-
- export const getOne = async (req,res) => {
     try {
-     const  postId = req.params.id
-     const post = await PostModel.findOne({_id: postId});
-     res.json(post);
-    } 
-    catch (error) {
-     console.log(error)
-     res.status(500).json({
-         message:"Невдалося знайти товар",
-     });
-    }
- }
- 
- export const remove = async (req, res) => {
-    try {
-      const postId = req.params.id;
-      const result = await PostModel.findOneAndDelete({ _id: postId });
-      if (!result) {
-        return res.status(404).json({
-          message: "Товар не знайдено",
+        const posts = await PostModel.find();
+        res.json(posts);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            message: "Невдалося знайти товари",
         });
-      }
-      res.json({
-        success: true,
-      });
-    } catch (error) {
-      console.log(error);
-      res.status(500).json({
-        message: "Невдалося видалити товар",
-      });
     }
-  };
+};
 
-
-  export const update = async (req, res) => {
+export const getOne = async (req, res) => {
     try {
-      const postId = req.params.id;
-      await PostModel.updateOne(
-        { _id: postId },
-        {
-          title : req.body.title,
-          text1 : req.body.text1,
-          text2 : req.body.text2,
-          price : req.body.price,
-          category: req.body.category,
-          imgmain:req.body.imgmain,
-          imgsecond:req.body.imgsecond,
-          imgthird:req.body.imgthird
-        }
-        );
-      res.json({
-        success: true,
-      });
+        const postId = req.params.id;
+        const post = await PostModel.findOne({_id: postId});
+        res.json(post);
     } catch (error) {
-      console.log(error);
-      res.status(500).json({
-        message: "Невдалося обновити дані товару",
-      });
+        console.log(error);
+        res.status(500).json({
+            message: "Невдалося знайти товар",
+        });
     }
-  };
+};
 
+export const remove = async (req, res) => {
+    try {
+        const postId = req.params.id;
+        const result = await PostModel.findOneAndDelete({_id: postId});
 
-export const create = async (req,res) => {
-   try {
-    const doc = new PostModel ({
-        title : req.body.title,
-        text1 : req.body.text1,
-        text2 : req.body.text2,
-        price : req.body.price,
-        category: req.body.category,
-        imgmain:req.body.imgmain,
-        imgsecond:req.body.imgsecond,
-        imgthird:req.body.imgthird
-      //  user: req._userId*/
-    })
-    const post = await doc.save()
-    res.json(post);
-   } 
-   catch (error) {
-    console.log(error)
-    res.status(500).json({
-        message:"Невдалося створити товар",
-    });
-   }
-}
+        if (!result) {
+            return res.status(404).json({
+                message: "Товар не знайдено",
+            });
+        }
+
+        res.json({
+            success: true,
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            message: "Невдалося видалити товар",
+        });
+    }
+};
+
+export const update = async (req, res) => {
+    try {
+        const postId = req.params.id;
+
+        const updateData = req.body.isVariant
+            ? {
+                price: req.body.price,
+                imgmain: req.body.imgmain,
+                imgsecond: req.body.imgsecond,
+                imgthird: req.body.imgthird,
+                title: req.body.title,
+                text1: req.body.text1,
+                text2: req.body.text2,
+                category: req.body.category,
+                isVariant: true,
+                variants: req.body.variants,
+            }
+            : {
+                title: req.body.title,
+                text1: req.body.text1,
+                text2: req.body.text2,
+                price: req.body.price,
+                category: req.body.category,
+                imgmain: req.body.imgmain,
+                imgsecond: req.body.imgsecond,
+                imgthird: req.body.imgthird,
+                isVariant: false
+            };
+
+        await PostModel.updateOne(
+            {_id: postId},
+            updateData
+        );
+
+        res.json({
+            success: true,
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            message: "Невдалося обновити дані товару",
+        });
+    }
+};
+
+export const create = async (req, res) => {
+    try {
+        const docData = req.body.isVariant
+            ? {
+                price: req.body.price,
+                imgmain: req.body.imgmain,
+                imgsecond: req.body.imgsecond,
+                imgthird: req.body.imgthird,
+                title: req.body.title,
+                text1: req.body.text1,
+                text2: req.body.text2,
+                category: req.body.category,
+                isVariant: true,
+                variants: req.body.variants,
+            }
+            : {
+                title: req.body.title,
+                text1: req.body.text1,
+                text2: req.body.text2,
+                price: req.body.price,
+                category: req.body.category,
+                imgmain: req.body.imgmain,
+                imgsecond: req.body.imgsecond,
+                imgthird: req.body.imgthird,
+                isVariant: false
+            };
+
+        const doc = new PostModel(docData);
+        const post = await doc.save();
+        res.json(post);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            message: "Невдалося створити товар",
+        });
+    }
+};
--- models/Post.js
+++ models/Post.js
@@ -1,42 +1,66 @@
-import { timeStamp } from "console";
 import mongoose from "mongoose";
-const  PostSchema = new mongoose.Schema({
- title : {
-    type:String,
-    required:true,
- },
- text1 : {
-    type:String,
-    required:true,
- },
- text2 : {
-   type:String,
-   required:true,
-},
- price : {
-    type:Number,
-    required:true,
-    default:0,
- },
- category : {
-   type:Number,
-   required:true,
-},
-  imgmain:String,
-  imgsecond:String,
-  imgthird:String
-// ids : {
-//    type:Number,
-//    required:true,
-// },
-//  user : {
-//    type : mongoose.Schema.Types.ObjectId,
-//    ref: "User",
-//    required:true,
-//  },
-},
-{
-timestamps:true,
-}
-)
-export default mongoose.model("Post", PostSchema)
+
+const VariantSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    price: {
+        type: Number,
+        required: true,
+    },
+    imgmain: String,
+    imgsecond: String,
+    imgthird: String
+});
+
+const PostSchema = new mongoose.Schema({
+        title: {
+            type: String,
+            required: true,
+        },
+        text1: {
+            type: String,
+            required: true,
+        },
+        text2: {
+            type: String,
+            required: true,
+        },
+        price: {
+            type: Number,
+            required: false,
+            default: 0,
+        },
+        category: {
+            type: Number,
+            required: true,
+        },
+        imgmain: {
+            type: String,
+            required: false,
+        },
+        imgsecond: {
+            type: String,
+            required: false,
+        },
+        imgthird: {
+            type: String,
+            required: false,
+        },
+        isVariant: {
+            type: Boolean,
+            default: false
+        },
+        variants: [VariantSchema],
+        selectedVariantIndex: {
+            type: Number,
+            default: 0
+        }
+    },
+    {
+        timestamps: true,
+    }
+);
+
+export default mongoose.model("Post", PostSchema);
